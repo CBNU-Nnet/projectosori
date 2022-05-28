8f6d1c580b8796ca22aa7f21d2a7aedd373731cb
Seungyeon Ryu <stbu4158@gmail.com>
2022-05-28 15:54:55 +0900

    modified views.py

--- outfit/views.py
+++ outfit/views.py
@@ -7,32 +7,30 @@ import requests
 
 # 구현 순서 -> 스크래핑 함수 구현 -> 객체 생성 함수 구현
 
+# 참고: https://coding-kindergarten.tistory.com/37
 
-def get_musinsa_request(key):
-    result = []
+
+def get_musinsa_request():
     url = "https://www.musinsa.com/mz/brandsnap?p=1"
-
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    print(type(soup))
-
+    return soup
 
 # 이미지 url 스크래핑 함수 구현
-# 참고: https://softwaree.tistory.com/74
 
 
-def fetch_img_data():
-    pass
-    # soup.set 객체에 img 태그 안의 src 속성 값 저장
-    # 0번째 요소는 스타일과 관련 없는 링크이므로 저장하지 않아야 함
-    # soup.set = soup.find_all('img', {'src': ''})
+def get_musinsa_images(soup):
+    list_items = soup.select('div.articleImg>a>img')
+    list_images = [i.attrs['src'] for i in list_items]
+    return list_images
 
-    # 제대로 스크래핑되었는지 확인
-    # for image in soup.set[1:]:
-    # print(image['src'])
 
-    # 상세 페이지 스크래핑 함수 구현
+# 상세 페이지 스크래핑 함수 구현
+def get_musinsa_links(soup):
+    list_items = soup.select('div.articleImg>a')
+    list_links = [i.attrs['href'] for i in list_items]
+    return list_links
 
-    # 이미지 객체 생성 함수 구현
+# 이미지 객체 생성 함수 구현
 
-    # 상세 페이지 객체 생성 함수 구현
+# 상세 페이지 객체 생성 함수 구현
